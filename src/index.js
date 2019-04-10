ba97ee037ee23a6e16cd9d74c9ab6de32810826a
Ivaylo Papazov <papazov.ivaylo@gmail.com>
2019-04-10 18:51:26 +0300

    Add rendering engine and base layout

--- src/index.js
+++ src/index.js
@@ -1,7 +1,19 @@
 const express = require('express');
+const handlebars = require('express-handlebars');
 
 const app = express();
 
-app.use('/public', express.static('./public'));
+app.engine('hbs', handlebars({
+    defaultLayout: 'main',
+    extname: '.hbs'
+}));
+
+app.set('view engine', 'hbs');
+
+app.use(express.static('./public'));
+
+app.get('/', (req, res) => {
+    res.render('home/index');
+});
 
 app.listen(3000, () => console.log(`Listening on port 3000...`));
