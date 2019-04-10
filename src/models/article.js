f275286ce2931d78acdd9d2a23cc23ced0c50d91
Ivaylo Papazov <papazov.ivaylo@gmail.com>
2019-04-10 21:12:25 +0300

    Fixed bug with article schema

--- src/models/article.js
+++ src/models/article.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const articleSchema = new mongoose.Schema({
     title: String,
     content: String,
-    Author: String
+    author: String
 });
 
 const Article = mongoose.model('Articles', articleSchema);
